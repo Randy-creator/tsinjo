c924ac739be9ca932640465514207a3eecacf3b6
Randy-creator <hei.randy.2@gmail.com>
2025-08-05 15:31:33 +0300

    fix: format

--- src/main/java/com/tsinjo/app/models/Beneficiary.java
+++ src/main/java/com/tsinjo/app/models/Beneficiary.java
@@ -3,7 +3,6 @@ package com.tsinjo.app.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
